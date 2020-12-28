8a199225443a8e699f8d43b17b2db453a3486fa7
ramiro <ramihertz@gmail.com>
2020-12-27 22:29:00 -0400

    se agrego typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'ramiro',
+    password: 'ramiro',
+    database: 'nest2',
+    autoLoadEntities: true,
+    synchronize: true,
+  })],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
