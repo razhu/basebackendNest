97e6ed19a96198a2edad83fe109a89ba18ee43a6
ramiro <rhuayller@agetic.gob.bo>
2021-01-06 11:48:11 -0400

    limt offset

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -5,7 +5,7 @@ import { CreateCoffeeDto } from './dto/create-coffee-dto';
 import { UpdateCoffeeDto } from './dto/update-coffee-dto';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
-
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 @Injectable()
 export class CoffeesService {
     constructor(
@@ -14,10 +14,13 @@ export class CoffeesService {
         @InjectRepository(Flavor)
         private readonly flavorRepository: Repository<Flavor>
     ){}
-    findAll() {
+    findAll(paginationQuery: PaginationQueryDto) {
+        const {limit, offset } = paginationQuery
         return this.coffeeRepository.find(
             {
-                relations: ['flavors']
+                relations: ['flavors'],
+                skip: offset,
+                take: limit
             }
         )
     }
