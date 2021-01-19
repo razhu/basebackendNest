b23258106930aaa1f9d2ac8dc63465e645ddf74f
ramiro <rhuayller@agetic.gob.bo>
2021-01-19 09:23:11 -0400

    se agrego .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingService } from './coffee-rating/coffee-rating.service';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [CoffeesModule, TypeOrmModule.forRoot({
+  imports: [
+    ConfigModule.forRoot(),
+    CoffeesModule,
+    TypeOrmModule.forRoot({
     type: 'postgres',
     host: 'localhost',
     port: 5432,
