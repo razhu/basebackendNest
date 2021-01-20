6f22cd1a557b03f1eb364287e54c5b1d6c053a1f
ramiro <rhuayller@agetic.gob.bo>
2021-01-20 10:22:58 -0400

    se agrego interceptor para formar respuesta de requests. ahora es con data {}

---
+++ src/common/interceptors/wrap-response.interceptor.ts
@@ -0,0 +1,12 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+
+@Injectable()
+export class WrapResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log('Before everything else...');
+    // return next.handle().pipe(tap(data => console.log('After ......', data)))
+    return next.handle().pipe(map((data) => ({data})))
+  }
+}
