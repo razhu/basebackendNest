c36f0a1fa6685da804c996a1c53f42762a6f565b
ramiro <rhuayller@agetic.gob.bo>
2021-01-20 14:07:36 -0400

    se agrego la parte de swagger

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter'
 import { ApiKeyGuard } from './common/guards/api-key.guard';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,17 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new WrapResponseInterceptor(), new TimeoutInterceptor());
   // app.useGlobalGuards(new ApiKeyGuard())
+  
+  // document your api
+  const options = new DocumentBuilder()
+  .setTitle('iLoveCoffee')
+  .setDescription('this coffee is not that bad')
+  .setVersion('1.0')
+  .build();
+ 
+ const document = SwaggerModule.createDocument(app, options); 
+ SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
